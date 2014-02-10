08769206ba3de0dc58e141916f9c8c682b09a2f2
Graziella <gra.galea@gmail.com>
2014-02-10 23:40:23 +0100

    changes done while testing

--- GuineaPig/src/Trial1.java
+++ GuineaPig/src/Trial1.java
@@ -1,8 +1,15 @@
 import java.applet.*;
 public class Trial1 {
 
+private static final int param2 = 45;
+
 	public int M1(){
-		int x = 15;
+		int x = param2 + 15;
+		return 0;
+	}
+
+	public int M2(){
+		int x = r + f+ g + 15;
 		return 0;
 	}
 }
